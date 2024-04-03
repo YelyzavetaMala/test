ab16c42fa50c773089bf181aef0d5cb7484d1984
YelyzavetaMala <malaelizaveta678@gmail.com>
2024-04-03 18:14:03 +0300

    kujlljdi

--- test.js
+++ test.js
@@ -15,4 +15,39 @@ toTop.onclick = function () {
       });
 }
 
+
+
+window.focus();
+
+let camera, scene, renderer;
+let world;
+let lastTime;
+let stack;
+let overhangs;
+const boxHeight = 1;
+const originalBoxSize = 3;
+let autopilot;
+let gameEnded;
+let robotPrecision;
+
+const scrolElement = document.getElementById("score");
+const instructionsElement = document.getElementById("instructions");
+const recultsElement = document.getElementById("recults");
+
+init();
+
+function setRobotPrecision() {
+    robotPrecision = Math.random() * 1 - 0.5;
+}
+
+function init() {
+    autopilot = true;
+    gameEnded = false;
+    lastTime = 0;
+    stack = [];
+    overhangs = [];
+    setRobotPrecision();
+}
+
+
     
